575fa836ae19144c94141cf5c4187dc9b6cf37f9
Nur-M-Arief-K <ariefnur141@gmail.com>
2023-07-03 11:43:22 +0700

    generate jwt access token and refresh token for postRegister

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,35 +1,68 @@
 import { createUser } from "../services/auth.service.js";
+import { generateToken } from "../services/token.service.js";
 
 export const postRegister = async (req, res, next) => {
-    try {
-        const { name, email, picture, status, password } = req.body;
-        const newUser = await createUser({ name, email, picture, status, password });
-        res.send( newUser );
-    } catch (error) {
-        next(error);
-    }
-}
+  const { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } = process.env;
+
+  try {
+    const { name, email, picture, status, password } = req.body;
+    const newUser = await createUser({
+      name,
+      email,
+      picture,
+      status,
+      password,
+    });
+    const access_token = await generateToken(
+      { userId: newUser._id },
+      "1d",
+      ACCESS_TOKEN_SECRET
+    );
+    const refresh_token = await generateToken(
+      { userId: newUser._id },
+      "30d",
+      REFRESH_TOKEN_SECRET
+    );
+
+    res.cookie("refreshtoken", refresh_token, {
+        httpOnly: true,
+        path: "api/v1/auth/refreshtoken",
+        maxAge: 30 * 24 * 40 * 60 * 1000 // 30 days
+    })
+    
+    res.json({
+        message: "user register success",
+        access_token,
+        user: {
+            _id: newUser._id,
+            name: newUser.name,
+            email: newUser.email,
+            picture: newUser.picture,
+            status: newUser.status,
+        }
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const postLogin = async (req, res, next) => {
-    try {
-        
-    } catch (error) {
-        next(error);
-    }
-}
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const postLogout = async (req, res, next) => {
-    try {
-        
-    } catch (error) {
-        next(error);
-    }
-}
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const postRefreshToken = async (req, res, next) => {
-    try {
-        
-    } catch (error) {
-        next(error);
-    }
-}
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
