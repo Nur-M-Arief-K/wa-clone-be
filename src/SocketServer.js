5bd0a7bd7743f0684cf22854b965752c7315dd2c
Nur-M-Arief-K <ariefnur141@gmail.com>
2023-07-18 19:59:47 +0700

    setup call user

--- src/SocketServer.js
+++ src/SocketServer.js
@@ -10,6 +10,9 @@ export default function (socket, io) {
     }
     // send online users to frontend
     io.emit("get-online-users", onlineUsers);
+
+    // send socket id
+    io.emit("setup socket", socket.id);
   });
 
   // Socket disconnect
@@ -43,5 +46,28 @@ export default function (socket, io) {
 
   socket.on("stop typing", (conversationId) => {
     socket.in(conversationId).emit("stop typing");
+  });
+
+  // User call
+  socket.on("call user", (data) => {
+    const userToCall = data.userToCall;
+    const userToCallSocketId = onlineUsers.find((user) => user.userId == userToCall);
+    io.to(userToCallSocketId?.socketId).emit("call user", {
+      // Send back caller's information
+      signal: data.signal,
+      from: data.from,
+      name: data.name,
+      picture: data.picture,
+    });
+  });
+
+  //---answer call
+  socket.on("answer call", (data) => {
+    io.to(data.to).emit("call accepted", data.signal);
+  });
+
+  //---end call
+  socket.on("end call", (receiverSocketId) => {
+    io.to(receiverSocketId).emit("end call");
   });
 }
